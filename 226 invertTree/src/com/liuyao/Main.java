4e7ba98ee750999bb3629ef2c89e3f103aca70e4
shushu1234 <liuyao9642@qq.com>
2018-05-11 18:06:16 +0800

    add 226

---
+++ 226 invertTree/src/com/liuyao/Main.java
@@ -0,0 +1,75 @@
+package com.liuyao;
+
+/**
+ *
+ 翻转一棵二叉树。
+
+ 示例：
+
+ 输入：
+
+ 4
+ /   \
+ 2     7
+ / \   / \
+ 1   3 6   9
+ 输出：
+
+ 4
+ /   \
+ 7     2
+ / \   / \
+ 9   6 3   1
+ 备注:
+ 这个问题是受到 Max Howell 的 原问题 启发的 ：
+
+ 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        TreeNode treeNode0 = new TreeNode(4);
+        TreeNode treeNode1 = new TreeNode(2);
+        TreeNode treeNode2 = new TreeNode(7);
+        TreeNode treeNode3 = new TreeNode(1);
+        TreeNode treeNode4 = new TreeNode(3);
+        TreeNode treeNode5 = new TreeNode(6);
+        TreeNode treeNode6 = new TreeNode(9);
+
+        treeNode0.left = treeNode1;
+        treeNode0.right = treeNode2;
+
+        treeNode1.left = treeNode3;
+        treeNode1.right = treeNode4;
+
+        treeNode2.left = treeNode5;
+        treeNode2.right = treeNode6;
+        new Solution().invertTree(treeNode0);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    static class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) {
+                return null;
+            } else {
+                TreeNode temp = root.left;
+                root.left = root.right;
+                root.right = temp;
+                invertTree(root.left);
+                invertTree(root.right);
+                return root;
+            }
+        }
+    }
+}
