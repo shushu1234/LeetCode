272fa0ee5967405cd71fb4afcf13f88c89827082
shushu1234 <liuyao9642@qq.com>
2018-05-16 23:58:06 +0800

    add 1

--- 01 TwoSum/src/Main.java
+++ 01 TwoSum/src/Main.java
@@ -1,5 +1,19 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
+/**
+ * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+
+ 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+
+ 示例:
+
+ 给定 nums = [2, 7, 11, 15], target = 9
+
+ 因为 nums[0] + nums[1] = 2 + 7 = 9
+ 所以返回 [0, 1]
+ */
 public class Main {
 
     public static void main(String[] args) {
@@ -56,6 +70,20 @@ class Solution {
             l=k;
         }
         return new int[]{l,n};
+    }
+
+    public int[] twoSum2(int[] numbers, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                result[1] = i + 1;
+                result[0] = map.get(target - numbers[i]);
+                return result;
+            }
+            map.put(numbers[i], i + 1);
+        }
+        return result;
     }
 }
 
