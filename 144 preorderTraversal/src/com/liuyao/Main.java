38e77a81ab1af6fb835b5219d811128c0deb9402
shushu1234 <liuyao9642@qq.com>
2018-07-16 20:55:40 +0800

    add 144

--- 144 preorderTraversal/src/com/liuyao/Main.java
+++ 144 preorderTraversal/src/com/liuyao/Main.java
@@ -1,9 +1,27 @@
 package com.liuyao;
 
+
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
+/**
+ *
+ 给定一个二叉树，返回它的 前序 遍历。
+
+ 示例:
+
+ 输入: [1,null,2,3]
+ 1
+ \
+ 2
+ /
+ 3
+
+ 输出: [1,2,3]
+ 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ */
 public class Main {
 
     public static void main(String[] args) {
