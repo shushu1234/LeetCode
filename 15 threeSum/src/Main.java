d53c0d52e749c0826526f7042c317876edf5d997
shushu1234 <liuyao9642@qq.com>
2018-09-02 18:54:00 +0800

    add 15

--- 15 threeSum/src/Main.java
+++ 15 threeSum/src/Main.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
@@ -21,7 +19,8 @@ import java.util.List;
  */
 public class Main {
     public static void main(String[] args) {
-
+        int[] ints = {1};
+        System.out.println(Solution.threeSum2(ints));
     }
 
     static class Solution {
@@ -59,6 +58,52 @@ public class Main {
                 }
             }
             return res;
+        }
+
+        public static List<List<Integer>> threeSum2(int[] nums) {
+            List<List<Integer>> lists = new LinkedList<>();
+            if (null == nums || nums.length == 0) {
+                return lists;
+            }
+
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (map.containsKey(nums[i])) {
+                    map.put(nums[i], (map.get(nums[i])) + 1);
+                } else {
+                    map.put(nums[i], 1);
+                }
+            }
+
+            if (map.containsKey(0) && map.get(0) >= 3) {
+                lists.add(Arrays.asList(0, 0, 0));
+            }
+
+            Arrays.sort(nums);
+
+            for (int i = 1, j = 1; i < nums.length; i++) {
+                if (nums[i] != nums[i - 1]) {
+                    nums[j] = nums[i];
+                    j++;
+                }
+            }
+            for (int i = 0; i < map.size(); i++) {
+                for (int j = i + 1; j < map.size(); j++) {
+                    if (nums[i] * 2 + nums[j] == 0 && map.get(nums[i]) >= 2) {
+                        lists.add(Arrays.asList(nums[i], nums[i], nums[j]));
+                    }
+
+                    if (nums[i] + nums[j] * 2 == 0 && map.get(nums[j]) >= 2) {
+                        lists.add(Arrays.asList(nums[i], nums[j], nums[j]));
+                    }
+
+                    int c = 0 - nums[i] - nums[j];
+                    if (c > nums[j] && map.containsKey(c)) {
+                        lists.add(Arrays.asList(nums[i], nums[j], c));
+                    }
+                }
+            }
+            return lists;
         }
     }
 }
