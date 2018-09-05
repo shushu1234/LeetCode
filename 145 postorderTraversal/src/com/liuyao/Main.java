66dafc3f5b4207fd01d51ab379193a52afd9ccc1
shushu1234 <liuyao9642@qq.com>
2018-09-05 16:31:20 +0800

    add 145

--- 145 postorderTraversal/src/com/liuyao/Main.java
+++ 145 postorderTraversal/src/com/liuyao/Main.java
@@ -5,25 +5,24 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- *
- 给定一个二叉树，返回它的 后序 遍历。
-
- 示例:
-
- 输入: [1,null,2,3]
- 1
- \
- 2
- /
- 3
-
- 输出: [3,2,1]
- 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 给定一个二叉树，返回它的 后序 遍历。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [1,null,2,3]
+ * 1
+ * \
+ * 2
+ * /
+ * 3
+ * <p>
+ * 输出: [3,2,1]
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
  */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         TreeNode treeNode0 = new TreeNode(8);
         TreeNode treeNode1 = new TreeNode(8);
         TreeNode treeNode2 = new TreeNode(7);
@@ -42,14 +41,19 @@ public class Main {
 //        System.out.println(Solution.postorderTraversal(treeNode0));
         System.out.println(Solution1.postorderTraversal(treeNode0));
     }
-     public static class TreeNode {
-          int val;
-          TreeNode left;
-          TreeNode right;
-          TreeNode(int x) { val = x; }
-      }
-    class Solution{
-        class Command{
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        class Command {
             String s;
             TreeNode node;
 
@@ -58,26 +62,27 @@ public class Main {
                 this.node = node;
             }
         }
-        public  List<Integer> inorderTraversal(TreeNode root) {
-            LinkedList<Integer> linkedList=new LinkedList<>();
-            if (root==null){
+
+        public List<Integer> inorderTraversal(TreeNode root) {
+            LinkedList<Integer> linkedList = new LinkedList<>();
+            if (root == null) {
                 return linkedList;
             }
 //            模拟系统栈调用
-            Stack<Command> stack=new Stack<>();
-            stack.push(new Command("go",root));
-            while (!stack.isEmpty()){
-                Command command=stack.pop();
-                if (command.s.equals("print")){
+            Stack<Command> stack = new Stack<>();
+            stack.push(new Command("go", root));
+            while (!stack.isEmpty()) {
+                Command command = stack.pop();
+                if (command.s.equals("print")) {
                     linkedList.add(command.node.val);
-                }else {
+                } else {
 //                    与递归的相反
-                    if (command.node.right!=null){
-                        stack.push(new Command("go",command.node.right));
+                    if (command.node.right != null) {
+                        stack.push(new Command("go", command.node.right));
                     }
-                    stack.push(new Command("print",command.node));
-                    if (command.node.left!=null){
-                        stack.push(new Command("go",command.node.left));
+                    stack.push(new Command("print", command.node));
+                    if (command.node.left != null) {
+                        stack.push(new Command("go", command.node.left));
                     }
 
                 }
@@ -87,18 +92,27 @@ public class Main {
 
     }
 
-    static  class Solution1 {
-        public static List<Integer> postorderTraversal(TreeNode root) {
+    class Solution1 {
+        public List<Integer> postorderTraversal(TreeNode root) {
             List<Integer> list=new LinkedList<>();
             Stack<TreeNode> stack=new Stack<>();
-            while (!stack.isEmpty()||root!=null){
-                while (root !=null){
+            TreeNode pre = null;
+            while(!stack.isEmpty() || root != null) {
+
+                while(root != null) {
                     stack.push(root);
-                    root=root.left;
+                    root = root.left;
                 }
-                root=stack.pop();
-                list.add(root.val);
-                root=root.right;
+
+                root = stack.peek();
+                if(root.right != null && root.right != pre) {
+                    root = root.right;
+                }else {
+                    root = stack.pop();
+                    list.add(root.val);
+                    pre = root;
+                    root = null;
+                }
             }
             return list;
         }
