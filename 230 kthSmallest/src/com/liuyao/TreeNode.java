2448ba870db4fa024ad57825f2233bf42950bc5a
shushu1234 <liuyao9642@qq.com>
2018-09-08 17:25:25 +0800

    add 230

---
+++ 230 kthSmallest/src/com/liuyao/TreeNode.java
@@ -0,0 +1,15 @@
+package com.liuyao;
+
+/**
+ * @author liuyao
+ * @date 2018/09/08
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
